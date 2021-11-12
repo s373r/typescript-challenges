4ce6fe53e359f9095168b1ebdc262eb248dd1739
Dima Pristupa <dima.pristupa@pm.me>
2021-11-12 13:17:31 +0200

    🏷️ Finish "new-skills"

--- src/new-skills/new-skills.ts
+++ src/new-skills/new-skills.ts
@@ -21,9 +21,19 @@ interface Teacher {
     teach(): string;
 }
 
-type SkilledPerson = Singer;
+type SkilledPerson = Singer & Dancer & Teacher;
 
-const johnDoe: SkilledPerson = {}
+const johnDoe: SkilledPerson = {
+    dance(): string {
+        return 'I can dance!';
+    },
+    sing(): string {
+        return 'I can sing!';
+    },
+    teach(): string {
+        return 'I can teach!';
+    },
+}
 
 function buildResume(person: SkilledPerson) {
     const skills = [
